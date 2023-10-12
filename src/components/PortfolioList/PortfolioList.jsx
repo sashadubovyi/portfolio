07283ef04fce5caa25adc9ea6248e64cf9a18c43
sashadubovyi <125696737+sashadubovyi@users.noreply.github.com>
2023-10-12 10:44:11 +0300

    add file

--- src/components/PortfolioList/PortfolioList.jsx
+++ src/components/PortfolioList/PortfolioList.jsx
@@ -4,65 +4,24 @@ import imgHA from '../../images/health_app.png';
 import imgSC from '../../images/sweet_choco.png';
 import imgWS from '../../images/web_studio.png';
 import s from './PortfolioList.module.css';
+import portfolioData from './portfolioData';
 
 const PortfolioList = () => {
   return (
     <ul className={s.workList}>
-      <li className={s.workItem}>
-        <a
-          className={s.workA}
-          href="https://sashadubovyi.github.io/BudgetBoss/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={imgBB} alt="budget boss" />
-        </a>
-        <p className={s.workP}>PREVIEW</p>
-      </li>
-      <li className={s.workItem}>
-        <a
-          className={s.workA}
-          href="https://m1lky4.github.io/error-masters-project/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={imgBS} alt="book shelf" />
-        </a>
-        <p className={s.workP}>PREVIEW</p>
-      </li>
-      <li className={s.workItem}>
-        <a
-          className={s.workA}
-          href="https://sashadubovyi.github.io/Sweet-Chocolate/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={imgSC} alt="sweet choco" />
-        </a>
-        <p className={s.workP}>PREVIEW</p>
-      </li>
-      <li className={s.workItem}>
-        <a
-          className={s.workA}
-          href="https://struzhaks.github.io/health-app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={imgHA} alt="health app" />
-        </a>
-        <p className={s.workP}>PREVIEW</p>
-      </li>
-      <li className={s.workItem}>
-        <a
-          className={s.workA}
-          href="https://sashadubovyi.github.io/WebStudio/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={imgWS} alt="web studio" />
-        </a>
-        <p className={s.workP}>PREVIEW</p>
-      </li>
+      {portfolioData.map(data => (
+        <li className={s.workItem} key={data.id}>
+          <a
+            className={s.workA}
+            href={data.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.image} alt={data.title} />
+          </a>
+          <p className={s.workP}>PREVIEW</p>
+        </li>
+      ))}
     </ul>
   );
 };
---
+++ src/components/PortfolioList/portfolioData.js
@@ -0,0 +1,47 @@
+import BB from '../../images/budget_boss.png';
+import BS from '../../images/book_shelf.png';
+import SC from '../../images/sweet_choco.png';
+import HA from '../../images/health_app.png';
+import WS from '../../images/web_studio.png';
+import DD from '../../images/ddod.png';
+
+const portfolioData = [
+  {
+    id: 1,
+    title: 'Budget Boss',
+    image: BB,
+    url: 'https://sashadubovyi.github.io/BudgetBoss/',
+  },
+  {
+    id: 2,
+    title: 'Book Shelf',
+    image: BS,
+    url: 'https://m1lky4.github.io/error-masters-project/',
+  },
+  {
+    id: 3,
+    title: 'Sweet Chocolate',
+    image: SC,
+    url: 'https://sashadubovyi.github.io/Sweet-Chocolate/',
+  },
+  {
+    id: 4,
+    title: 'Health App',
+    image: HA,
+    url: 'https://struzhaks.github.io/health-app/',
+  },
+  {
+    id: 5,
+    title: 'Web Studio',
+    image: WS,
+    url: 'https://sashadubovyi.github.io/WebStudio/',
+  },
+  {
+    id: 6,
+    title: 'DDoD',
+    image: DD,
+    url: 'https://playfuture.games/',
+  },
+];
+
+export default portfolioData;
